7216c1681c83fed09d04ff65c45fb3b3704564e5
Glenn Conrad <gconz@triunelogic.com>
2021-05-22 16:47:30 -0500

    Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'a239bd25671f1a75f891af4b732f2510'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '8c4de794-97bb-42d3-a125-6d74a0607dee'
+};
